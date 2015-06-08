90b45c7930ec996265139b922caaccaee964a88c
BuikiKaesu <Nameless73@mail.ru>
2015-06-09 00:36:11 +0400

    tabulation

--- find_missed.rb
+++ find_missed.rb
@@ -1,25 +1,25 @@
 class Array
 	def find_missed
-  	result = []
-  	missed = (last - first + 1) - size
-  	if self.size == 2
-  		result << self[0] + 1 
-  		result << self[0] + 2 if self[-1] - self[0] == 3 # if missed elements follow each other
-  	else
-  		mid = size % 2 == 0 ? size / 2 : (size+1) / 2 # find mid element
-  		shift = self[mid - 1] - (mid) - (self[0] - 1) # find shift
-	  	case shift
-	  		when 0 # when missing elements on the right
-	  			result += self[mid-1..-1].find_missed
-	  		when 1 # when missing elements on both sides
-	  		  result += self[mid-1..-1].find_missed if missed > 1
-	  		  result += self[0..mid-1].find_missed 
-	  		when 2 # when missing elements on the left
-	  			result += self[0..mid-1].find_missed
-	  	end
-	  end
-	  result
-  end
+		result = []
+		missed = (last - first + 1) - size
+		if self.size == 2
+			result << self[0] + 1 
+			result << self[0] + 2 if self[-1] - self[0] == 3 # if missed elements follow each other
+		else
+			mid = size % 2 == 0 ? size / 2 : (size+1) / 2 # find mid element
+			shift = self[mid - 1] - (mid) - (self[0] - 1) # find shift
+			case shift
+				when 0 # when missing elements on the right
+					result += self[mid-1..-1].find_missed
+				when 1 # when missing elements on both sides
+					result += self[mid-1..-1].find_missed if missed > 1
+					result += self[0..mid-1].find_missed 
+				when 2 # when missing elements on the left
+					result += self[0..mid-1].find_missed
+			end
+		end
+		result
+	end
 end
 
 array = (1..1000000).to_a
